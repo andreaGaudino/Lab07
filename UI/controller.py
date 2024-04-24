7e53bfa1ef7ea4e089487151a1165b9d5c10c5b4
andreaGaudino <andrea.gaudino02@gmail.com>
2024-04-24 16:07:41 +0200

    inizio secondo punto

--- UI/controller.py
+++ UI/controller.py
@@ -48,30 +48,75 @@ class Controller:
 
         self._mese = int(self._view.dd_mese.value)
         situazioni = Model().get_situazione()
-        def cerca_percorso(dizio):
-            if len(dizio) == 15:
-                return dizio
-            elif len(dizio)==0:
+        def cerca_percorso(dizio, citta_iniziale):
+
+            '''elif len(dizio)==0:
                 migliore = None
                 for situa in situazioni:
                     if situa.data.month == self._mese:
                         if migliore is None or (situa.data<=migliore.data and situa.umidita<migliore.umidita):
                             migliore = situa
-                dizio[migliore.data] = migliore
+                dizio[migliore.data] = (migliore, 1)
+                for situa in situazioni:
+                    if situa.data.month == self._mese:
+                        if situa.localita == migliore.localita and (situa.data-list(dizio.keys())[-1]).days == 1 and list(dizio.values())[-1][1] <=3 :
+                            dizio[situa.data] = (situa, list(dizio.values())[-1][1]+1)
                 return cerca_percorso(dizio)
             else:
                 ultima_data = list(dizio.keys())[-1]
                 migliore = None
                 for situa in situazioni:
-                    if situa.data.month == self._mese:
+                    if situa.data.month == self._mese and situa.data>ultima_data:
                         if migliore is None or (situa.data>ultima_data and situa.data<=migliore.data and situa.umidita<migliore.umidita):
-                            migliore = situa
+                            numero = 0
+                            for situazione in dizio:
+                                if situazione.localita == situa.localita:
+                                    numero = dizio[situazione][1]
+                            if numero < 6:
+                                migliore = (situa, numero)
+                            else:
+                                return cerca_percorso(dizio)
+
                 dizio[migliore.data] = migliore
-                return cerca_percorso(dizio)
-        d = cerca_percorso({})
-        for i in d:
-            self._view.lst_result.controls.append(ft.Text(f"{i}, {d[i]}"))
-        self._view.update_page()
+                print(len(dizio))
+                return cerca_percorso(dizio)'''
+            new_situazioni = {}
+            for situa in situazioni:
+                if situa.data.month == self._mese:
+                    if situa.data in list(new_situazioni.keys()):
+                        new_situazioni[situa.data].append(situa)
+                    else:
+                        new_situazioni[situa.data] = [situa]
+            if len(dizio) >= 15:
+                return dizio
+            elif len(dizio) == 0:
+                citta = None
+                for i in list(new_situazioni.values())[0]:
+                    if i.localita == citta_iniziale:
+                        citta = i
+                dizio[citta.data] = (citta, 1)
+                for j in range(1,3):
+                    for i in list(new_situazioni.values())[j]:
+                        if i.localita==citta.localita and ((i.data-citta.data).days==1 or (i.data-citta.data).days==2):
+                            dizio[i.data] = (i, list(dizio.values())[-1][1]+1)
+            else:
+                ultima =  list(dizio.values())[-1]
+                ultima_data = ultima[0].data
+                migliore = None
+                if ultima[1] < 6 and len(dizio)<3:
+                    pass #continua da qua
+
+
+
+
+
+
+        for citta in ["Torino", "Milano", "Genova"]:
+            d = cerca_percorso({}, citta)
+            for i in d:
+                self._view.lst_result.controls.append(ft.Text(f"{d[i]}"))
+            self._view.update_page()
+
 
     def read_mese(self, e):
         self._mese = int(e.control.value)
